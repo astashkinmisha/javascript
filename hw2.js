062eeee8d54ea1abd880f086b7e285568b92af13
misha <trickerforyou@gmail.com>
2020-10-29 21:13:56 +0200

    v1

---
+++ hw2.js
@@ -0,0 +1,127 @@
+
+
+
+//let firstArray  = ['danny','ozzy', 'cody', 'iren', 'banana'];
+//
+// console.log(firstArray);
+_________________
+
+
+ let secondArray = [ 234, 432, 455, 678, 12];
+
+ console.log(secondArray);
+
+_________________
+
+//
+// let thirdArray = ['Aaron', 'Michael', 345, 44, false];
+//
+// console.log(thirdArray);
+//
+_________________
+
+//
+
+
+//let randomArray = ['intro', 'cranch,', 13, true,];
+//
+// console.log(randomArray[2]);
+//
+_________________
+
+//for(let i=1; i<10; i++){
+//    document.write("<div> 123 </div>");
+//
+//}
+_________________
+
+//
+//for(let i=1; i<10; i++){
+//    document.write('<div>', 'teration', +i ,'</div');
+//
+//}
+_________________
+
+
+//let i = 0;
+//
+//while (i < 20, i++) {
+//
+//    document.write('<h1> iteration', 'i+1,</h1>');
+//
+//}
+_________________
+
+
+//const array =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+////
+//// console.log(array);
+////
+_________________
+
+//
+//for (let i = 0; i <= 10; i = i + 1) {
+//    console.log(array[i]);
+//
+//
+//}
+_________________
+
+//const strArray =['carmen', 'tribolt', 'repress', 'cisco', 'agent', 'fake', 'kingston,', 'odlo', 'samurai', 'ronin'];
+////console.log(strArray);
+//
+//for (let i = 0; i < 10; i=i+1){
+//    console.log(strArray[i]);
+//}
+_________________
+
+           //task 14,15,16
+  same just 'string' and Number
+//const diffArray =[10, true, 'Simba', false, 'function', 'true', true, 'getback', 'igor', 'sambo'];
+////console.log(diffArray);
+//
+//for (let i = 0; i<10; i=i+1) {
+//    if (typeof diffArray[i] === 'boolean') {
+//        console.log(diffArray[i]);
+//    }
+//}
+_________________
+
+//          task 17
+//const group =[];
+//group.push(10, true, 'Simba', false, 'function', 'true', true, 'getback', 'igor', 'sambo');
+//
+//for (let i = 0; i < 10; i=i+1) {
+//    console.log(group[i]);
+//}
+_________________
+
+//for (let i = 0; i < 10; i=i+1){
+//    console.log(i);
+//    document.write(i);
+//}
+//
+_________________
+
+//
+//for (let i = 0; i < 100; i=i+1){
+//    console.log(i);
+//    document.write(i);
+//}
+//
+_________________
+
+//for (let i = 0; i < 100; i=i+2){
+//    console.log(i);
+//    document.write(i);
+//}
+_________________
+
+//task 24
+
+//for (let i = 0; i < 59; i=i+1){
+//   console.log(i);
+//for (let i=0; i < 59; i=i+1){
+//   console.log(i);
+//}
+_________________
